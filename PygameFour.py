f19c2045d9dac2bbb79db71a400a4846d34f7fac
ghtydnty1 <ghtydnty1@gmail.com>
2020-07-07 15:48:26 +0900

    pygame

---
+++ PygameFour.py
@@ -0,0 +1,32 @@
+#점들을 잇는 선그리기
+import random
+import sys
+import pygame
+from pygame.locals import QUIT
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400,300))
+FPSCLOCK = pygame.time.Clock()
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        SURFACE.fill((0,0,0))
+
+        pointlist= []
+        for _ in range(10):
+            xpos = random.randint(0,400)
+            ypos = random.randint(0,300)
+            pointlist.append((xpos,ypos))
+
+        pygame.draw.lines(SURFACE,(255,255,255),True,pointlist, 5)
+
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+if __name__ == '__main__':
+    main()
