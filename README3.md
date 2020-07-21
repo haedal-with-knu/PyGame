a29c5e940939dc9c4fe8747033a9a116f0277d8b
ghtydnty1 <ghtydnty1@gmail.com>
2020-07-21 18:26:12 +0900

    pygame

---
+++ README3.md
@@ -0,0 +1,76 @@
+
+출처: 게임으로 배우는 파이썬(저자: 다나카 겐이치로)
+
++ [주요코드 먼저 알아보기](./PygamePrac/PygameZero.py)
+
+
++ [배경에 사각형 그리기](./PygamePrac/PygameOne.py)
+
+ ![사각형그리기](./img/rect.PNG)
+
++ [배경에 원,타원그리기](./PygamePrac/PygameTwo.py)
+
+![원그리기](./img/원.PNG)
+
+
++ [배경에 선,격자무늬 그리기](./PygamePrac/PygameThree.py)
+
+![선 그리기](./img/선그리기.PNG)
+![격자무늬 그리기](./img/격자무늬.PNG)
+
+
+
++ [배경에 임의의점을 잇는 선그리기](./PygamePrac/PygameFour.py)
+
+![점들을 잇는 선그리기](./img/선잇기.PNG)
+
+
+
++ [폴라곤그리기](./PygamePrac/PygameFive.py)
+
+
+
+![폴라곤그리기](./img/폴라곤그리기.PNG)
+
+
+
++ [이미지파일불러오고 회전시키기](./PygamePrac/PygameSix.py)
+
+
+![이미지불러오기](./img/이미지불러오기.PNG)
+
+
+
++ [글자불러오고 줌인회전시키기](./PygamePrac/PygameSeven.py)
+
+
+![글자불러오기](./img/글자불러오기.PNG)
+
+
+
++ [마우스로 점찍기](./PygamePrac/PygameEight.py)
++ [마우스이동으로 글 혹은 그림 그리기](./PygamePrac/PygameNine.py)
+
+
+
+![마우스로 점찍기](./img/마우스포인터.PNG)
+
+
+
++ [키다운으로 이미지 옮기기](./PygamePrac/PygameTen.py)
+
+
+
+![키다운으로 이미지 옮기기](./img/움직이는사진.PNG)
+
+
+
++ [Cos,Sin이용 별모양 그리기](./PygamePrac/PygameEleven.py)
+
+
+
+![별모양 그리기](./img/별모양만들기.PNG)
+
+
+
++ [클래스와 상속에 관한 코드](./PygamePrac/PygameTwelve.py)
--- game.py
+++ game.py
@@ -1,37 +1,52 @@
 import sys
-from random import *
 import pygame
 from pygame.locals import *
 
 pygame.init()
-pygame.key.set_repeat(5,5)
-SURFACE = pygame.display.set_mode((600,600))
+W = 800
+H = 600
+SURFACE = pygame.display.set_mode((W,H))
 FPSCLOCK = pygame.time.Clock()
+pygame.display.set_caption('Game : 공피하기')
+default_font = pygame.font.Font('NanumGothic.ttf',28)
+empty_list =[]
+BALL = empty_list((242,242,0), Rect(300,400,20,20),10)
 
-def main():
-    charicter_image = pygame.image.load("./PySpaceship/spaceship.png")
-    obstacle_image = pygame.image.load("./img/start.png")
-    sysfont = pygame.font.SysFont(None,36)
-    score = 0
-    velocity = 0
-    hole = []
-    game_over = False
+
+def draw_text(text, font, surface, x, y, main_color):
+    text_obj = font.render(text, True,  main_color)
+    text_rect = text_obj.get_rect()
+    text_rect.centerx = x
+    text_rect.centery = y
+    surface.blit(text_obj, text_rect)
+
+def ingame():
+    global score
+    myfont = pygame.font.SysFont(None,80)
+    mess_clear = myfont.render("CLEARED!", True, (255,255,0))
+    mess_over = myfont.render("GAME OVER!",True, (255,255,0))
+
+    haedal = pygame.image.load('fighter2.png')
+
+def F_Screen():
+    start_image = pygame.image.load('haedal.jpg')
     while True:
-        is_space_down = False
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
-                sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_SPACE:
-                    is_space_down = True
+                sys.exit() #나중에 없앨 코드
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_q:
+                    print('qqq')
+                elif event.key == pygame.K_ESCAPE:
+                    return 'quit'
+        SURFACE.fill((255,255,255))
+        SURFACE.blit(start_image, (0, 0))
+        draw_text("공 피하기 게임", pygame.font.Font('NanumGothic.ttf',90), SURFACE, W/2, H/3.4, (0,0,0))
+        draw_text("시작하시려면 Q를 눌러주세요", default_font, SURFACE, W / 2, H / 2, (0, 0, 0))
+        draw_text("종료하시려면 ESC를 눌러주세요", default_font, SURFACE, W / 2, H / 1.3, (0, 0, 0))
+        pygame.display.update()
+        FPSCLOCK.tick(3)
 
-        #캐릭이동
-        if not game_over:
-            if #캐릭이 장애물을 뛰어넘엇을때
-                score += 10
-
-
-            #동굴스크롤
-            edge = holes[-1].copy()
-            test = edge.move
+if __name__ == "__main__":
+    F_Screen()
